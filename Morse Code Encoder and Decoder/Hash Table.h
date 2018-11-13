ea53b640d30b949ca80d89407a854256be957628
Harrison <31551456+HarrisonLara@users.noreply.github.com>
2018-11-13 12:34:06 -0600

    Add hash table

---
+++ Morse Code Encoder and Decoder/Hash Table.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class CodeHash {
+private:
+
+	unordered_map<string, string> map;
+
+public:
+
+	CodeHash() {}
+
+	void add_letter(string value, string code) {
+		map[value] = code;
+	};
+
+	string get_morse_version(string value) {
+		return map[value];
+	};
+};
