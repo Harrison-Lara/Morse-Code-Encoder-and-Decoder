8a364401d01dfc239e3c3cbf7cddfc9aa4c6db1c
Harrison <31551456+HarrisonLara@users.noreply.github.com>
2018-11-15 21:19:53 -0600

    Add member variables to converter

--- Morse Code Encoder and Decoder/Converter.h
+++ Morse Code Encoder and Decoder/Converter.h
@@ -1,9 +1,20 @@
+#include "MorseTree.h"
+#include "HashTable.h"
 #pragma once
 
 class Converter {
-
 private:
 
+	MorseTree morse_tree;
+	CodeHash morse_hash;
+
 public:
-	
+	Converter() {
+		morse_tree = MorseTree();
+	}
+
+	void parse();
+	string search(BTNode *&node, string in);
+	string decode(string in);
+	string encode(string in);
 };
